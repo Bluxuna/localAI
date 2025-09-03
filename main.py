718b99cb0553dc3c6d37d5970a603295dce21ac2
FELINISY <ninikheladze9@gmail.com>
2025-09-03 14:26:57 +0400

    initial commit

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from chooseAI.linux_system_information import LinuxSystemInformation
+from chooseAI.windows_system_information import WindowsSystemInformation
 from chooseAI.systemInfo import SystemInformation
 
-linux_info = LinuxSystemInformation()
-cpu_info = linux_info.get_cpu()
+windows_info = WindowsSystemInformation()
+cpu_info = windows_info.get_cpu()
 print(f"CPU: {cpu_info.name} - {cpu_info.logical_cores} cores")
 if cpu_info.error:
     print(f"Error: {cpu_info.error}")
